c667296d07dfaa5865bdf72bf4b42f4cd10f286a
Aman Gupta <aman.gupta.rkg6@gmail.com>
2022-05-08 02:50:37 +0530

    converted to PWA

---
+++ server/client/public/serviceWorker.js
@@ -0,0 +1,30 @@
+let CACHE_NAME = 'my-site-cache-v1';
+
+const urlsToCache = [
+    '/',
+    '/index.html',
+];
+
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
